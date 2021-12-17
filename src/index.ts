9f6aeb1aa7e74d44ad6dd7775bfd0a8d17078a60
Juan Sebastian Agudelo <juansac.me@gmail.com>
2021-12-17 15:58:17 -0500

    Refactor index with mongo

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import express, { Application, Request, Response } from 'express';
+import { config } from 'dotenv'
+import express, { Application } from 'express'
+import { MongoClient } from 'mongodb'
 
 const app: Application = express()
+config()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+const mongoURI = process.env.MONGO_URI ?? ''
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello world and GDSC!!! 🚀')
+app.get('/', (_req, res) => {
+  MongoClient.connect(mongoURI, (err, db) => {
+    if (err) {
+      res.status(500).send('💥  BOOM: No se conecto la db 💥')
+    } else {
+      res.send('Me conecté a la db! 😎')
+      // @ts-ignore
+      db.close()
+    }
+  })
 })
 
 app.listen(3000, () => console.log("I'm alive on localhost:3000"))
